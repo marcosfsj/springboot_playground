1eb903ec35ccbc1fd769d799c3fda6f1bd3c63dc
marcfernandes <marcfernandes@teksystems.com>
2019-11-15 08:45:32 -0500

    Adds a new jpa rest project

---
+++ spring-data-jpa-rest/src/main/java/com/training/springdatajparest/SpringDataJpaRestApplication.java
@@ -0,0 +1,13 @@
+package com.training.springdatajparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaRestApplication.class, args);
+	}
+
+}
